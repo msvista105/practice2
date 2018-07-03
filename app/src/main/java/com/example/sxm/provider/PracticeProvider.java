9f0d7d6f2071de81e9ad2b6b443255ac2ed02534
sunxiaoming <sxml105@gmail.com>
2018-07-03 19:27:04 +0800

    数据库操作 -- 待完成

---
+++ app/src/main/java/com/example/sxm/provider/PracticeProvider.java
@@ -0,0 +1,69 @@
+package com.example.sxm.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class PracticeProvider extends ContentProvider {
+    private static final int VERSION = 1;//数据库版本
+    private static final String DATABASE_NAME = "practice.db";
+    private static final String TABLE_SYSTEMUI = "systemui";
+    @Override
+    public boolean onCreate() {
+
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    private class DataBaseHelper extends SQLiteOpenHelper {
+        private Context mContext = null;
+
+        public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+            mContext = context;
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE "+TABLE_SYSTEMUI);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+}
