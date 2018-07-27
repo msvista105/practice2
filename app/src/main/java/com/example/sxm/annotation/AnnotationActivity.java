8a5c45f709b17f3d468a26e562ffae28c1f005f7
sunxiaoming <sxml105@gmail.com>
2018-07-27 13:30:39 +0800

    简单的注解小练习-注解的定义及使用

---
+++ app/src/main/java/com/example/sxm/annotation/AnnotationActivity.java
@@ -0,0 +1,57 @@
+package com.example.sxm.annotation;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.sxm.practice.R;
+import com.example.sxm.utils.LogUtils;
+
+import java.lang.reflect.Method;
+
+public class AnnotationActivity extends AppCompatActivity {
+    private static final String TAG = "AnnotationActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_annotation);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        processDemo(Demo.class.getName());
+    }
+
+    private void processDemo(String clsName){
+        LogUtils.d(TAG,"processDemo clsName:"+clsName);
+        Class target = null;
+        Class annotation = MyTag.class;
+        try {
+            target = Class.forName(clsName);
+            for (Method m:target.getDeclaredMethods()){
+                if(m.isAnnotationPresent(annotation)){
+                    LogUtils.d(TAG,String.format("processDemo %s annotated by %s",m.getName(),annotation));
+                }else {
+                    LogUtils.d(TAG,String.format("processDemo %s  not annotated by %s",m.getName(),annotation));
+                }
+            }
+        }catch (Exception e){}
+    }
+
+}
---
+++ app/src/main/java/com/example/sxm/annotation/Demo.java
@@ -0,0 +1,9 @@
+package com.example.sxm.annotation;
+
+public class Demo{
+    void test1(){}
+    @MyTag(name = "test2" ,age = 30, vollage = 4)
+    void test2(){}
+    @MyTag(name = "test3" ,age = 31, vollage = 3)
+    void test3(){}
+}
